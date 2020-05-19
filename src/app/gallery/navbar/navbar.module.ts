016607df0e54ec43173e5eaf6bf2110c8c2285df
rong <rong161514@gmail.com>
2020-05-19 16:43:40 +0800

    + navbar

--- src/app/gallery/navbar/navbar.module.ts
+++ src/app/gallery/navbar/navbar.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class NavbarModule { }
--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { MatRippleModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [PageNotFoundComponent],
   imports: [
     CommonModule,
+    RouterModule,
     MatRippleModule
   ]
 })
