a37315cccb80589d273e486134e1e97a986dc704
rong <rong161514@gmail.com>
2020-05-21 09:44:45 +0800

    add trigger animation to card-con

--- src/app/gallery/panel/panel.component.ts
+++ src/app/gallery/panel/panel.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.scss']
+  styleUrls: ['./panel.component.scss'],
+  animations: [
+    trigger('controlCardCon', [
+      state(
+        'open',
+        style({
+          width: '57%',
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          width: 0,
+          opacity: 0,
+          visibility: 'hidden',
+        })
+      ),
+      transition('open<=>closed', [animate('0.3s')]),
+    ]),
+  ],
 })
 export class PanelComponent implements OnInit {
+  isOpen = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  trigger() {
+    this.isOpen = !this.isOpen;
   }
-
 }
