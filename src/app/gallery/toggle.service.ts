ba7b66e63789750b915e0bc6f226efade61c3af7
wutai <1615140542@qq.com>
2020-09-26 19:57:16 +0800

    focus or not depend on the env

--- src/app/gallery/toggle.service.ts
+++ src/app/gallery/toggle.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ToggleService {
   focusOnArticle: BehaviorSubject<boolean>;
 
   constructor() {
-    this.focusOnArticle = new BehaviorSubject<boolean>(false);
+    this.focusOnArticle = new BehaviorSubject<boolean>(!environment.production);
   }
 
   toggle() {
